29785d33daa8544e527f41181c8584bcbfe7cc54
Vikil Naik <vikilnaik@Vikils-MacBook-Pro.local>
2021-01-12 12:47:15 -0500

    The timers have now been updated to correlate with the songs currenttime as well as the duration of the song

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,4 @@
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
 	faPlay,
@@ -18,13 +18,30 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
             audioRef.current.play();
             setIsPlaying(!isPlaying);
         }
+    };
+
+    const timeUpdateHandler = (e) => {
+        const current = e.target.currentTime;
+        const duration = e.target.duration;
+        setSongInfo({...songInfo, currentTime: current, duration})
     }
+
+    const getTime = (time) => {
+        return(
+            Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+        )
+    }
+        // State
+        const [songInfo, setSongInfo] = useState({
+            currentTime: null,
+            duration: null,
+        });
 	return (
 		<div className="player">
 			<div className="time-control">
-				<p>Start Time</p>
+				<p>{getTime(songInfo.currentTime)}</p>
 				<input type="range" />
-				<p>End Time</p>
+				<p>{getTime(songInfo.duration)}</p>
 			</div>
 			<div className="play-control">
 				<FontAwesomeIcon
@@ -39,7 +56,7 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
 					icon={faAngleRight}
 				/>
 			</div>
-            <audio ref={audioRef} src={currentSong.audio}></audio>
+            <audio onLoadedMetadata={timeUpdateHandler} onTimeUpdate={timeUpdateHandler} ref={audioRef} src={currentSong.audio}></audio>
 		</div>
 	);
 };
