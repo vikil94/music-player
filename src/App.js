d5dca802b48c0f5385fabf9a4ae90ac23e8eb1f5
Vikil Naik <vikilnaik@Vikils-MBP.hitronhub.home>
2021-01-11 22:53:28 -0500

    imported state into app.cs and now the cover, songtitle, and artist will show up via the currentSong state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 //Import Styles
 import './styles/app.scss';
 // Adding Components
 import Player from './components/Player';
 import Song from './components/Song';
+// import Util
+import data from './util';
 
 
 function App() {
+  // State
+ const [songs, setSongs] = useState(data());
+ const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
