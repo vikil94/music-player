a35ce28de80313331c9b2aa0664834209ebd8255
Vikil Naik <vikilnaik@Vikils-MBP.hitronhub.home>
2021-01-13 16:52:00 -0500

    Songs do no play automatically when using the skip forawrd or skip back button, this will be the next thing that is fixed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Song from "./components/Song";
 import Library from "./components/Library";
 import Nav from "./components/Nav";
 // import Util
-import data from "./util";
+import data from "./data";
 
 function App() {
 	// Ref
